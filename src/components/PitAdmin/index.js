1c4c3dd5d24ea9caa6ee391efcb9d81ce1d0d1b4
Mason DiGiorgio <31572670+MasonDiGi@users.noreply.github.com>
2020-03-10 14:46:44 -0400

    Finalized everything

---
+++ src/components/PitAdmin/index.js
@@ -0,0 +1,139 @@
+import React, { Component } from 'react';
+// Bootstrap
+import { Table } from "react-bootstrap";
+import Jumbotron from "react-bootstrap/Jumbotron";
+// My Stuff
+import { withAuthorization } from "../Session";
+
+class AdminPage extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			loading: false,
+			table: {scoutData: {}, pitData: {}},
+		};
+		if (this.props.cookies.get('sid')) {
+			this.id = this.props.cookies.get('sid').substring(0, this.props.cookies.get('sid').search('@'));
+		}
+	}
+	componentDidMount() {
+		this.setState({loading: true});
+		this.props.firebase.table('/').on('value', snapshot => {
+			const table = snapshot.val();
+			this.setState({
+				table: table,
+				loading: false,
+			});
+		});
+		if (this.id) {
+			this.props.firebase.user(this.id).on('value', snapshot => {
+				this.setState({role: snapshot.val()['role']});
+			});
+		}
+	}
+	componentWillUnmount() {
+		this.props.firebase.users().off();
+	}
+	render() {
+		const { table, loading } = this.state;
+		return (
+			<div>
+				{loading && <div>Loading ...</div>}
+				<TablePrint table={table} />
+			</div>
+		);
+	}
+}
+
+const TablePrint = ({ table }) => (
+	<div>
+	<Jumbotron className="mx-3 mx-sm-5 my-3 py-5 bg-secondary">
+		<h1>Match Scouting</h1>
+		<Table responsive striped bordered hover>
+			<thead>
+			<tr>
+				<th>Match #</th>
+				<th>Team #</th>
+				<th>Balls in Auton</th>
+				<th>High Goals in Auton</th>
+				<th>Low Goals in Auton</th>
+				<th>Balls in Teleop</th>
+				<th>High Goals in Teleop</th>
+				<th>Low Goals in Teleop</th>
+				<th>Robot Speed</th>
+				<th>Successful Climb</th>
+			</tr>
+			</thead>
+			<tbody>
+			{ Object.keys(table.scoutData).map((user) => {
+				return (
+						Object.keys(user).map((match) => {
+							let matchAndTeam = Object.keys(table.scoutData[user])[match];
+							let currentObj = table.scoutData[user][matchAndTeam];
+							if (matchAndTeam) {
+								const matchNum = matchAndTeam.substring(0, matchAndTeam.search('-'));
+								const teamNum = matchAndTeam.substring(matchAndTeam.search('-') + 1);
+								return (
+									<tr>
+										<th>{matchNum}</th>
+										<th>{teamNum}</th>
+										<th>{currentObj['autonBallsPickedUp']}</th>
+										<th>{currentObj['autonHighGoals']}</th>
+										<th>{currentObj['autonLowGoals']}</th>
+										<th>{currentObj['teleopBallsPickedUp']}</th>
+										<th>{currentObj['teleopHighGoals']}</th>
+										<th>{currentObj['teleopLowGoals']}</th>
+										<th>{currentObj['teleopRobotSpeed']}</th>
+										<th>{currentObj['successfulClimb']}</th>
+									</tr>
+								);
+							} else {return null}
+						})
+				);
+			})}
+			</tbody>
+		</Table>
+	</Jumbotron>
+	<Jumbotron className="mx-3 mx-sm-5 my-3 py-5 bg-secondary">
+	<h1>Pit Scouting</h1>
+		<Table responsive striped bordered hover>
+			<thead>
+			<tr>
+				<th>Team #</th>
+				<th>High Goal?</th>
+				<th>Low Goal?</th>
+				<th>Long Range?</th>
+				<th>Medium Range?</th>
+				<th>Short Range?</th>
+				<th>Wheel of Fortune?</th>
+			</tr>
+			</thead>
+			<tbody>
+			{ Object.keys(table.pitData).map((user) => {
+				return (
+					Object.keys(user).map((match) => {
+						let teamNum = Object.keys(table.pitData[user])[match];
+						if (teamNum) {
+							let currentObj = table.pitData[user][teamNum];
+							return (
+								<tr>
+									<th>{currentObj['teamNumber']}</th>
+									<th>{currentObj['highGoal']}</th>
+									<th>{currentObj['lowGoal']}</th>
+									<th>{currentObj['longRange']}</th>
+									<th>{currentObj['medRange']}</th>
+									<th>{currentObj['shortRange']}</th>
+									<th>{currentObj['wheelOfFortune']}</th>
+								</tr>
+							);
+						} else {return null}
+					})
+				);
+			})}
+			</tbody>
+		</Table>
+	</Jumbotron>
+	</div>
+);
+
+export default withAuthorization(AdminPage);
